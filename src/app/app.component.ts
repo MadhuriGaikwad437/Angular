0689c1146160c00d9dd37d10e4620d50a56e1573
unknown <madhurigaikwad437@gmail.com>
2018-07-09 17:25:17 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+	selector:'m-app',
+	template:'<h1>E-Online Shopping....'
+})
+
+export class FirstComponent
+{
+
+}
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+
+
+import{ platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { FirstModule } from './app/app.module';
+
+
+platformBrowserDynamic().bootstrapModule(FirstModule);
+
+
+
+
